6caffa6ad593b4a473d30d34e5b6f5fb25542064
Anirudh-Sathish <anirudhsathish@gmail.com>
2024-09-13 17:37:36 +0530

    Refactor: Refactor the test code

--- python/examples/reduction.py
+++ python/examples/reduction.py
@@ -12,37 +12,24 @@ CPU_BLOCK_SIZE = 4
 USE_GPU = False
 
 @triton.jit
-def reduction_kernel(x_ptr,  # *Pointer* to input vector.
-                     output_ptr,  # *Pointer* to output scalar (final sum).
-                     n_elements,  # Size of the vector.
-                     BLOCK_SIZE: tl.constexpr  # Number of elements each program processes.
+def reduction_kernel(x_ptr,
+                     output_ptr,
+                     n_elements,
+                     BLOCK_SIZE: tl.constexpr
                      ):
-    pid = tl.program_id(axis=0)  # Program ID (for block-level parallelism).
+    pid = tl.program_id(axis=0)
     block_start = pid * BLOCK_SIZE
     offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    
-    # Mask for boundaries of the input tensor.
     mask = offsets < n_elements
-    
-    # Load elements of the input tensor.
     x = tl.load(x_ptr + offsets, mask=mask, other=0.0)
-    
-    # Perform a block-wide reduction (sum within the block).
     partial_sum = tl.sum(x, axis=0)
-    
     tl.atomic_add(output_ptr, partial_sum)
 
-
 def reduction(x: torch.Tensor):
-    # Prepare output tensor (final result, scalar).
     output = torch.zeros(1, device='cpu', dtype=x.dtype)
-    
     n_elements = x.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
-    
-    # Launch the Triton kernel.
     reduction_kernel[grid](x, output, n_elements, BLOCK_SIZE=16)
-    
     return output.item()
 
 
